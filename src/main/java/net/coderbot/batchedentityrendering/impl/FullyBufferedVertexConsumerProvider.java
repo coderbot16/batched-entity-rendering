5c0d87d7e57a3d548a85d7c39be28f305fa841ea
coderbot <coderbot16@gmail.com>
2021-08-16 21:20:50 -0700

    Allow multiple active buffer builders again, enabling actual batching in practice
    We use 32 buffers, so in most scenes no fragmentation will occur.

--- src/main/java/net/coderbot/batchedentityrendering/impl/FullyBufferedVertexConsumerProvider.java
+++ src/main/java/net/coderbot/batchedentityrendering/impl/FullyBufferedVertexConsumerProvider.java
@@ -2,10 +2,11 @@ package net.coderbot.batchedentityrendering.impl;
 
 import com.mojang.blaze3d.platform.GlStateManager;
 import com.mojang.blaze3d.systems.RenderSystem;
+import it.unimi.dsi.fastutil.objects.Object2IntMap;
+import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap;
 import net.coderbot.batchedentityrendering.mixin.RenderLayerAccessor;
 
 import net.minecraft.client.render.BufferBuilder;
-import net.minecraft.client.render.BufferRenderer;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.VertexConsumerProvider;
@@ -15,10 +16,15 @@ import org.lwjgl.system.MemoryUtil;
 import java.nio.ByteBuffer;
 import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class FullyBufferedVertexConsumerProvider extends VertexConsumerProvider.Immediate implements MemoryTrackingBuffer {
-	private final SegmentedBufferBuilder builder;
+	private static final int UNASSIGNED_AFFINITY = -1;
+	private static final int NUM_BUFFERS = 32;
+
+	private final SegmentedBufferBuilder[] builders;
+	private final Object2IntMap<RenderLayer> affinities;
+	private int nextAffinity;
 	private int drawCalls;
 
 	public static FullyBufferedVertexConsumerProvider instance;
@@ -26,8 +32,16 @@ public class FullyBufferedVertexConsumerProvider extends VertexConsumerProvider.
 	public FullyBufferedVertexConsumerProvider() {
 		super(new BufferBuilder(0), Collections.emptyMap());
 
-		// 2 MB initial allocation
-		this.builder = new SegmentedBufferBuilder();
+		this.builders = new SegmentedBufferBuilder[NUM_BUFFERS];
+
+		for (int i = 0; i < this.builders.length; i++) {
+			this.builders[i] = new SegmentedBufferBuilder();
+		}
+
+		this.affinities = new Object2IntOpenHashMap<>();
+		this.affinities.defaultReturnValue(UNASSIGNED_AFFINITY);
+		this.nextAffinity = 0;
+
 		this.drawCalls = 0;
 
 		// TODO: Eh
@@ -36,17 +50,37 @@ public class FullyBufferedVertexConsumerProvider extends VertexConsumerProvider.
 
 	@Override
 	public VertexConsumer getBuffer(RenderLayer renderLayer) {
-		return builder.getBuffer(renderLayer);
+		int affinity = affinities.getInt(renderLayer);
+
+		if (affinity == UNASSIGNED_AFFINITY) {
+			if (nextAffinity < builders.length) {
+				affinity = nextAffinity;
+				nextAffinity += 1;
+			} else {
+				// TODO: Don't just select a random buffer.
+				affinity = ThreadLocalRandom.current().nextInt(builders.length);
+			}
+
+			affinities.put(renderLayer, affinity);
+		}
+
+		return builders[affinity].getBuffer(renderLayer);
 	}
 
 	@Override
 	public void draw() {
-		List<BufferSegment> segments = builder.getSegments();
+		// TODO: Proper sorting???
+		for (SegmentedBufferBuilder builder : builders) {
+			List<BufferSegment> segments = builder.getSegments();
 
-		for (BufferSegment segment : segments) {
-			drawCalls += 1;
-			draw(segment);
+			for (BufferSegment segment : segments) {
+				drawCalls += 1;
+				draw(segment);
+			}
 		}
+
+		affinities.clear();
+		nextAffinity = 0;
 	}
 
 	private static boolean isTranslucent(RenderLayer layer) {
@@ -87,11 +121,23 @@ public class FullyBufferedVertexConsumerProvider extends VertexConsumerProvider.
 
 	@Override
 	public int getAllocatedSize() {
-		return builder.getAllocatedSize();
+		int size = 0;
+
+		for (SegmentedBufferBuilder builder : builders) {
+			size += builder.getAllocatedSize();
+		}
+
+		return size;
 	}
 
 	@Override
 	public int getUsedSize() {
-		return builder.getUsedSize();
+		int size = 0;
+
+		for (SegmentedBufferBuilder builder : builders) {
+			size += builder.getUsedSize();
+		}
+
+		return size;
 	}
 }
