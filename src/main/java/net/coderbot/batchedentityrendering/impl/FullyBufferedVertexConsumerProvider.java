6182ea83fb2506573a837e413759f44a3dcad2fc
coderbot <coderbot16@gmail.com>
2021-08-18 20:53:04 -0700

    Order render layer draws wrt translucency again

--- src/main/java/net/coderbot/batchedentityrendering/impl/FullyBufferedVertexConsumerProvider.java
+++ src/main/java/net/coderbot/batchedentityrendering/impl/FullyBufferedVertexConsumerProvider.java
@@ -4,6 +4,9 @@ import com.mojang.blaze3d.platform.GlStateManager;
 import com.mojang.blaze3d.systems.RenderSystem;
 import it.unimi.dsi.fastutil.objects.Object2IntMap;
 import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap;
+import net.coderbot.batchedentityrendering.impl.ordering.RenderOrderManager;
+import net.coderbot.batchedentityrendering.impl.ordering.SimpleRenderOrderManager;
+import net.coderbot.batchedentityrendering.impl.ordering.TranslucencyRenderOrderManager;
 import net.coderbot.batchedentityrendering.mixin.RenderLayerAccessor;
 
 import net.minecraft.client.render.BufferBuilder;
@@ -14,14 +17,18 @@ import net.minecraft.client.render.VertexFormat;
 import org.lwjgl.system.MemoryUtil;
 
 import java.nio.ByteBuffer;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class FullyBufferedVertexConsumerProvider extends VertexConsumerProvider.Immediate implements MemoryTrackingBuffer {
 	private static final int UNASSIGNED_AFFINITY = -1;
 	private static final int NUM_BUFFERS = 32;
 
+	private final RenderOrderManager renderOrderManager;
 	private final SegmentedBufferBuilder[] builders;
 	private final Object2IntMap<RenderLayer> affinities;
 	private int nextAffinity;
@@ -32,6 +39,7 @@ public class FullyBufferedVertexConsumerProvider extends VertexConsumerProvider.
 	public FullyBufferedVertexConsumerProvider() {
 		super(new BufferBuilder(0), Collections.emptyMap());
 
+		this.renderOrderManager = new TranslucencyRenderOrderManager();
 		this.builders = new SegmentedBufferBuilder[NUM_BUFFERS];
 
 		for (int i = 0; i < this.builders.length; i++) {
@@ -50,6 +58,7 @@ public class FullyBufferedVertexConsumerProvider extends VertexConsumerProvider.
 
 	@Override
 	public VertexConsumer getBuffer(RenderLayer renderLayer) {
+		renderOrderManager.begin(renderLayer);
 		int affinity = affinities.getInt(renderLayer);
 
 		if (affinity == UNASSIGNED_AFFINITY) {
@@ -69,16 +78,28 @@ public class FullyBufferedVertexConsumerProvider extends VertexConsumerProvider.
 
 	@Override
 	public void draw() {
-		// TODO: Proper sorting???
+		Map<RenderLayer, List<BufferSegment>> layerToSegment = new HashMap<>();
+
 		for (SegmentedBufferBuilder builder : builders) {
 			List<BufferSegment> segments = builder.getSegments();
 
 			for (BufferSegment segment : segments) {
-				drawCalls += 1;
-				draw(segment);
+				layerToSegment.computeIfAbsent(segment.getRenderLayer(), (layer) -> new ArrayList<>()).add(segment);
 			}
 		}
 
+		for (RenderLayer layer : renderOrderManager.getRenderOrder()) {
+			layer.startDrawing();
+
+			for (BufferSegment segment : layerToSegment.getOrDefault(layer, Collections.emptyList())) {
+				drawInner(segment);
+				drawCalls += 1;
+			}
+
+			layer.endDrawing();
+		}
+
+		renderOrderManager.reset();
 		affinities.clear();
 		nextAffinity = 0;
 	}
@@ -96,11 +117,15 @@ public class FullyBufferedVertexConsumerProvider extends VertexConsumerProvider.
 	}
 
 	private static void draw(BufferSegment segment) {
+		segment.getRenderLayer().startDrawing();
+		drawInner(segment);
+		segment.getRenderLayer().endDrawing();
+	}
+
+	private static void drawInner(BufferSegment segment) {
 		BufferBuilder.DrawArrayParameters parameters = segment.getParameters();
 
-		segment.getRenderLayer().startDrawing();
 		draw(segment.getSlice(), parameters.getMode(), parameters.getVertexFormat(), parameters.getCount());
-		segment.getRenderLayer().endDrawing();
 	}
 
 	private static void draw(ByteBuffer buffer, int mode, VertexFormat vertexFormat, int count) {
