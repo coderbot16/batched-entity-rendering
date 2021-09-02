6594bb6d9d873f095428dafc6e11660bf8cadd79
coderbot <coderbot16@gmail.com>
2021-09-02 14:50:59 -0700

    Fix Carpet compatibility

--- src/main/java/net/coderbot/batchedentityrendering/mixin/MixinWorldRenderer.java
+++ src/main/java/net/coderbot/batchedentityrendering/mixin/MixinWorldRenderer.java
@@ -1,10 +1,8 @@
 package net.coderbot.batchedentityrendering.mixin;
 
 import net.coderbot.batchedentityrendering.impl.ExtendedBufferStorage;
-import net.coderbot.batchedentityrendering.impl.FlushableVertexConsumerProvider;
 import net.coderbot.batchedentityrendering.impl.FullyBufferedVertexConsumerProvider;
 import net.coderbot.batchedentityrendering.impl.Groupable;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.BufferBuilderStorage;
 import net.minecraft.client.render.Camera;
 import net.minecraft.client.render.Frustum;
@@ -13,9 +11,6 @@ import net.minecraft.client.render.LightmapTextureManager;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.WorldRenderer;
 import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.client.world.ClientWorld;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityType;
 import net.minecraft.util.math.Matrix4f;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.profiler.Profiler;
@@ -25,14 +20,8 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @Mixin(WorldRenderer.class)
 public class MixinWorldRenderer {
@@ -89,31 +78,5 @@ public class MixinWorldRenderer {
 	private void batchedentityrendering$endWorldRender(MatrixStack matrices, float tickDelta, long limitTime, boolean renderBlockOutline, Camera camera, GameRenderer gameRenderer, LightmapTextureManager lightmapTextureManager, Matrix4f matrix4f, CallbackInfo callback) {
 		((ExtendedBufferStorage) bufferBuilders).endWorldRendering();
 		groupable = null;
-	}
-
-	@Redirect(method = "render", at = @At(value = "INVOKE", target = "net/minecraft/client/world/ClientWorld.getEntities ()Ljava/lang/Iterable;"))
-	private Iterable<Entity> batchedentityrendering$sortEntityList(ClientWorld world) {
-		// Sort the entity list first in order to allow vanilla's entity batching code to work better.
-		Iterable<Entity> entityIterable = world.getEntities();
-
-		if (MinecraftClient.getInstance().player.isSneaking()) {
-			// TODO: Don't disable optimization when sneaking
-			return entityIterable;
-		}
-
-		world.getProfiler().push("sortEntityList");
-
-		Map<EntityType<?>, List<Entity>> sortedEntities = new HashMap<>();
-
-		List<Entity> entities = new ArrayList<>();
-		entityIterable.forEach(entity -> {
-			sortedEntities.computeIfAbsent(entity.getType(), entityType -> new ArrayList<>(32)).add(entity);
-		});
-
-		sortedEntities.values().forEach(entities::addAll);
-
-		world.getProfiler().pop();
-
-		return entities;
 	}
 }
