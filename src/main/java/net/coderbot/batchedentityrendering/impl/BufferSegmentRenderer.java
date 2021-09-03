f5948a5a385ea715b77dea6ad2a9853a4e6e2eb3
coderbot <coderbot16@gmail.com>
2021-09-02 18:16:24 -0700

    Remove unused methods in BufferSegmentRenderer

--- src/main/java/net/coderbot/batchedentityrendering/impl/BufferSegmentRenderer.java
+++ src/main/java/net/coderbot/batchedentityrendering/impl/BufferSegmentRenderer.java
@@ -3,8 +3,6 @@ package net.coderbot.batchedentityrendering.impl;
 import net.minecraft.client.render.BufferBuilder;
 import net.minecraft.client.render.BufferRenderer;
 
-import java.nio.ByteBuffer;
-
 public class BufferSegmentRenderer {
     private final BufferBuilder fakeBufferBuilder;
     private final BufferBuilderExt fakeBufferBuilderExt;
@@ -30,43 +28,5 @@ public class BufferSegmentRenderer {
         fakeBufferBuilderExt.setupBufferSlice(segment.getSlice(), segment.getParameters());
         BufferRenderer.draw(fakeBufferBuilder);
         fakeBufferBuilderExt.teardownBufferSlice();
-    }
-
-    private static void setInternalBuffer(BufferBuilder target, ByteBuffer buffer, BufferBuilder.DrawArrayParameters parameters) {
-        // target.buffer = buffer;
-        // target.parameters = new ArrayList<>(1);
-        // target.parameters.add(parameters);
-
-        // target.lastParameterIndex does not need modification.
-        // target.buildStart = parameters.getCount() * parameters.getVertexFormat().getVertexSize();
-        // target.elementOffset = target.buildStart;
-        // target.nextDrawStart is zero as it should be.
-
-        // target.vertexCount is never nonzero in this process.
-        // target.currentElement is never non-null in this process.
-        // target.currentElementId is never nonzero.
-        // target.drawMode is irrelevant.
-        // target.format is irrelevant.
-        // The final 3 booleans are also irrelvant.
-
-        // TODO
-    }
-
-    private static void unsetInternalBuffer(BufferBuilder target) {
-        // target.buffer = null;
-        // target.parameters gets reset.
-        // target.lastParameterIndex gets reset.
-        // target.buildStart gets reset.
-        // target.elementOffset gets reset.
-        // target.nextDrawStart gets reset.
-
-        // target.vertexCount is never nonzero in this process.
-        // target.currentElement is never non-null in this process.
-        // target.currentElementId is never nonzero.
-        // target.drawMode is irrelevant.
-        // target.format is irrelevant.
-        // The final 3 booleans are also irrelvant.
-
-        // TODO
     }
 }
